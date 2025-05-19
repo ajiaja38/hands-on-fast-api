5d2c2d99cf3469b59ab4c92360848038c994d32d
ajiaja38 <ajicooljazz38@gmail.com>
2025-05-19 16:24:19 +0700

    feat: update minor

--- src/main.py
+++ src/main.py
@@ -16,6 +16,6 @@ async def http_exception_handler(request: Request, exc: HTTPException):
 
 @apps.get("/", response_model=ResponseEntity[str])
 async def root():
-    return ResponseEntity(code=200, status="OK", message="Hello World")
+    return ResponseEntity(code=200, status="OK", message="Hello World from FastAPI")
   
 apps.include_router(router)
